8a448b107d1bafcaeeeb2227d2393d9f87476afc
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-02-10 00:52:00 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'components/App';
+import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import { Provider } from 'react-redux';
+import store, { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
